447d07dcfd0ccbe0a48eec98f032abad0026fe52
Nelson  Chang <102230885+NChang007@users.noreply.github.com>
2023-07-13 20:31:33 +0000

    backend stuff working

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,73 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+# comments = db.Table(
+#     'comments',
+#     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+#     db.Column('discussion_id', db.Integer, db.ForeignKey('discussion.id'), primary_key=True)
+# )
+
 class User(db.Model):
+    __tablename__= 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(256), unique=False, nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
+
+    discussions = db.relationship("Discussion", back_populates="created_by", foreign_keys='Discussion.user_id')
+    comments = db.relationship("Comment", back_populates="created_by", foreign_keys='Comment.user_id')
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.id}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Discussion(db.Model):
+    __tablename__= 'discussions'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), unique=False, nullable=False)
+    discussion = db.Column(db.String(500), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    created_by = db.relationship("User", back_populates="discussions", foreign_keys=[user_id])
+    disc_comments = db.relationship('Comment', backref='discussion', lazy=True)
+
+
+    def __repr__(self):
+        return f'<Discussion {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            'discussion': self.discussion,
+            'createdBy': self.created_by.serialize(),
+            'comments' : [comment.serialize() for comment in self.disc_comments]
+        }
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    discussion_id = db.Column(db.Integer, db.ForeignKey("discussions.id"), nullable=False)
+    comment = db.Column(db.String(500), unique=False, nullable=False)
+    
+    created_by = db.relationship("User", back_populates="comments", foreign_keys=[user_id])
+
+    def __repr__(self):
+        return f'<Comment {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # "discussion_id": self.discussion_id,
+            'created_by': self.created_by.serialize(),
+            'comment' : self.comment,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,10 +2,82 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Discussion, Comment
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
+
+# GET ALL USERS
+@api.route('/users', methods=['GET'])
+def getAllUsers():
+    users = User.query.all()
+    users_dic = [user.serialize() for user in users]
+    return jsonify(users_dic), 200
+
+# UPDATE USER
+@api.route("/updateUser", methods=["PUT"])
+# @jwt_required()
+def update_user():
+    # Retrieve the data from the request
+    data = request.json
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+
+    # Get the current user from the JWT identity
+    # user_id = get_jwt_identity()
+    user_id = 1
+    user = User.query.get(user_id)
+
+    # Update the user's profile
+    if name is not None:
+        user.name = name
+    if email is not None:
+        user.email = email
+    if password is not None:
+        user.password = password
+
+    db.session.commit()
+
+    # Return a success response
+    return jsonify({"message": "Profile updated successfully"}), 200
+
+# GET ALL DISCUSSIONS
+@api.route('/discussions', methods=['GET'])
+def getAllDiscussions():
+    discussions = Discussion.query.all()
+    discussions_dic = [discussion.serialize() for discussion in discussions]
+    return jsonify(discussions_dic), 200
+
+# CREATE DISCUSSION
+@api.route('/discussions', methods=['POST'])
+def createDiscussion():
+    user_id = 1
+    body = request.get_json()
+    new_disc = Discussion (
+        user_id=user_id,
+        title=body["title"],
+        discussion=body["discussion"]
+    )
+    db.session.add(new_disc)
+    db.session.commit()
+    return jsonify(new_disc.serialize()), 200
+
+# CREATE COMMENT
+@api.route('/comment', methods=['POST'])
+def createComment():
+    user_id = 1
+    discussion_id = 1
+    body = request.get_json()
+    new_comment = Comment (
+        user_id=user_id,
+        discussion_id=body["discussion_id"],
+        comment=body["comment"],
+    )
+    db.session.add(new_comment)
+    db.session.commit()
+    return jsonify(new_comment.serialize()), 200
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
